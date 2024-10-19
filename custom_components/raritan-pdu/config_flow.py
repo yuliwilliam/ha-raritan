9a51809a1b785330b50a99633d11353f485c7dfe
Lizhi Yu <leedsyu@yahoo.com>
2024-10-18 21:50:49 -0700

    update _async_abort_entries_match

--- custom_components/raritan-pdu/config_flow.py
+++ custom_components/raritan-pdu/config_flow.py
@@ -18,7 +18,9 @@ class RaritanPDUConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
-            self._async_abort_entries_match(user_input[CONF_HOST], user_input[CONF_PORT], user_input[CONF_COMMUNITY])
+            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST],
+                                             CONF_PORT: user_input[CONF_PORT],
+                                             CONF_COMMUNITY: user_input[CONF_COMMUNITY]})
 
             try:
                 pdu = RaritanPDU(user_input[CONF_HOST], user_input[CONF_PORT], user_input[CONF_COMMUNITY])
