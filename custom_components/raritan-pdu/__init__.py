a1772218d51861b528b2f62db6b2a56d61ac2564
Lizhi Yu <leedsyu@yahoo.com>
2024-10-18 20:16:59 -0700

    remove unused testing code

--- custom_components/raritan-pdu/__init__.py
+++ custom_components/raritan-pdu/__init__.py
@@ -1,12 +1,9 @@
 """Raritan PDU Integration."""
-from datetime import timedelta
-
 from homeassistant.components.snmp.const import CONF_COMMUNITY
 from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from raritan_pdu import RaritanPDU
 from const import _LOGGER, DOMAIN, PLATFORMS
@@ -32,57 +29,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Raritan PDU config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
     return True
-
-
-class RaritanPDUCoordinator(DataUpdateCoordinator):
-    """My custom coordinator."""
-
-    def __init__(self, hass, my_api):
-        """Initialize my coordinator."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            # Name of the data. For logging purposes.
-            name="My sensor",
-            # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=1),
-            # Set always_update to `False` if the data returned from the
-            # api can be compared via `__eq__` to avoid duplicate updates
-            # being dispatched to listeners
-            always_update=True
-        )
-        self.my_api = my_api
-        self._device: MyDevice | None = None
-
-    async def _async_setup(self):
-        """Set up the coordinator
-
-        This is the place to set up your coordinator,
-        or to load data, that only needs to be loaded once.
-
-        This method will be called automatically during
-        coordinator.async_config_entry_first_refresh.
-        """
-        self._device = await self.my_api.get_device()
-
-    async def _async_update_data(self):
-        """Fetch data from API endpoint.
-
-        This is the place to pre-process the data to lookup tables
-        so entities can quickly look up their data.
-        """
-        try:
-            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
-            # handled by the data update coordinator.
-            async with async_timeout.timeout(10):
-                # Grab active context variables to limit data required to be fetched from API
-                # Note: using context is not required if there is no need or ability to limit
-                # data retrieved from API.
-                listening_idx = set(self.async_contexts())
-                return await self.my_api.fetch_data(listening_idx)
-        except ApiAuthError as err:
-            # Raising ConfigEntryAuthFailed will cancel future updates
-            # and start a config flow with SOURCE_REAUTH (async_step_reauth)
-            raise ConfigEntryAuthFailed from err
-        except ApiError as err:
-            raise UpdateFailed(f"Error communicating with API: {err}")
