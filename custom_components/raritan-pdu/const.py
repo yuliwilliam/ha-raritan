2e6895ab56577d0ca7518974e47110915a60d384
Lizhi Yu <leedsyu@yahoo.com>
2024-10-18 22:21:50 -0700

    update path

--- custom_components/raritan-pdu/const.py
+++ custom_components/raritan-pdu/const.py
@@ -9,7 +9,7 @@ MANUFACTURER = "Raritan"
 
 CONF_COMMUNITY = "community"
 
-MIB_SOURCE_DIR = './mibs'
+MIB_SOURCE_DIR = f"./custom_components/{DOMAIN}/mibs"
 
 # PLATFORMS = [Platform.SENSOR, Platform.SWITCH]
 PLATFORMS = [Platform.SENSOR]
