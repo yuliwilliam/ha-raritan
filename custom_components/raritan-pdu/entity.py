7c1b48c2bb343d6cd056066fd9c3c4fbbfeb3265
Lizhi Yu <leedsyu@yahoo.com>
2024-10-22 19:56:05 -0700

    update

--- custom_components/raritan-pdu/entity.py
+++ custom_components/raritan-pdu/entity.py
@@ -28,13 +28,8 @@ class RaritanPDUEntity(CoordinatorEntity, Entity):
         if self.outlet_index > 0:
             self.outlet = self.coordinator.pdu.get_outlet_by_index(self.outlet_index)
 
-        self._attr_name = self.get_name()
-
     @property
-    def name(self) -> str | None:
-        return self.get_name()
-
-    def get_name(self) -> str:
+    def name(self) -> str:
         """Return the name of the entity."""
         if self.entity_description.name is not None:
             return self.entity_description.name
@@ -53,7 +48,7 @@ class RaritanPDUEntity(CoordinatorEntity, Entity):
     @property
     def unique_id(self) -> str:
         """Return the entity unique id."""
-        return f"{self.coordinator.pdu.name}-outlet-{self.outlet_index}-{self.entity_description.key}"
+        return f"{self.coordinator.pdu.name}-outlet-{self.outlet_index}-{self.entity_description.key}".replace(" ", "-").lower()
 
     @property
     def device_info(self) -> dict:
