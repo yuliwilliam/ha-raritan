8d41288098b5c1b403c6d1bd964b44bb9fcf4a1d
Lizhi Yu <leedsyu@yahoo.com>
2024-10-22 20:08:57 -0700

    update

--- custom_components/raritan-pdu/entity.py
+++ custom_components/raritan-pdu/entity.py
@@ -1,22 +1,18 @@
 from __future__ import annotations
 
-from typing import Optional
-
-from homeassistant.components.button import ButtonEntityDescription
-from homeassistant.components.sensor import SensorEntityDescription
-from homeassistant.components.switch import SwitchEntityDescription
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import Entity, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .raritan_pdu import RaritanPDUOutlet
 from .coordinator import RaritanPDUCoordinator
+from .const import _LOGGER
 
 
 class RaritanPDUEntity(CoordinatorEntity, Entity):
     """Dyson entity base class."""
 
     def __init__(self, coordinator: RaritanPDUCoordinator,
-                 description: SensorEntityDescription | SwitchEntityDescription | ButtonEntityDescription,
+                 description: EntityDescription,
                  outlet_index: int):
         """Initialize the entity."""
         super().__init__(coordinator)
@@ -28,12 +24,12 @@ class RaritanPDUEntity(CoordinatorEntity, Entity):
         if self.outlet_index > 0:
             self.outlet = self.coordinator.pdu.get_outlet_by_index(self.outlet_index)
 
-
         self._attr_device_info = self.coordinator.device_info
-        self._attr_unique_id = f"{self.coordinator.pdu.name}-outlet-{self.outlet_index}-{self.entity_description.key}".replace(" ", "-").lower()
+        self._attr_unique_id = f"{self.coordinator.pdu.name}-outlet-{self.outlet_index}-{self.entity_description.key}".replace(
+            " ", "-").lower()
 
         if self.entity_description.name is not None:
-            self._attr_name =  self.entity_description.name
+            self._attr_name = self.entity_description.name
         else:
             formatted_description_key = self.entity_description.key.replace('_', ' ')
             if self.outlet is not None:
@@ -41,9 +37,11 @@ class RaritanPDUEntity(CoordinatorEntity, Entity):
                 name_prefix = f"Outlet {self.outlet_index}"
                 if outlet_label != f"Outlet {self.outlet_index}":
                     name_prefix = f"{name_prefix} {outlet_label}"
-                self._attr_name =  f"{name_prefix} {formatted_description_key}"
+                self._attr_name = f"{name_prefix} {formatted_description_key}"
             else:
-                self._attr_name =  formatted_description_key
+                self._attr_name = formatted_description_key
+
+        _LOGGER.info(f"Finish setting up entity {self._attr_unique_id}")
 
     # @property
     # def name(self) -> str:
